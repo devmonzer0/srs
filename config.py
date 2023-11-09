cd5bafb8417bbdc0460a567b22cfca7fb76769d0
devmonzer0 <150098012+devmonzer0@users.noreply.github.com>
2023-11-09 11:28:29 +0200

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-
-    APP_ID = int(os.environ.get("APP_ID", 27477919))
-
-    API_HASH = os.environ.get("API_HASH", "b25cce1727f6d33d41d9e00e3ed62583")
